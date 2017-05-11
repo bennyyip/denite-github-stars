63eaf58a3184c368a6e0c736c2b4f7c1748afa8e
BennyYip <yebenmy@protonmail.com>
2017-05-11 21:23:06 +0800

    add nvim support

--- rplugin/python3/denite/source/denite-github-stars.py
+++ rplugin/python3/denite/source/denite-github-stars.py
@@ -1,15 +1,8 @@
 from urllib.request import urlopen
 import json
 import os
-import vim
 from wcwidth import wcswidth
 from .base import Base
-
-username = vim.vars['dgs#username'].decode()
-cache_dir = os.path.join(
-    os.path.expanduser(os.getenv('XDG_CACHE_HOME', '~/.cache')), 'githubstars')
-cache_dir = os.path.normpath(cache_dir)
-cache_file = os.path.join(cache_dir, 'starred_repos')
 
 
 class Source(Base):
@@ -17,9 +10,15 @@ class Source(Base):
         super().__init__(vim)
         self.name = 'github_stars'
         self.kind = 'file'
+        self.username = self.vim.vars['dgs#username']
+        cache_dir = os.path.join(
+            os.path.expanduser(os.getenv('XDG_CACHE_HOME', '~/.cache')),
+            'githubstars')
+        self.cache_dir = os.path.normpath(cache_dir)
+        self.cache_file = os.path.join(cache_dir, 'starred_repos')
 
     def gather_candidates(self, context):
-        repos = get_repos()
+        repos = self.get_repos()
         candidates = []
         for (name, url, desc) in repos:
             candidates.append({
@@ -29,38 +28,40 @@ class Source(Base):
             })
         return candidates
 
+    def get_repos(self):
+        if not os.path.exists(self.cache_dir):
+            os.makedirs(self.cache_dir)
+        if not os.path.exists(self.cache_file):
+            self.fetch_from_github()
+        return self.read_cache()
 
-def get_repos():
-    if not os.path.exists(cache_dir):
-        os.makedirs(cache_dir)
-    if not os.path.exists(cache_file):
-        fetch(username)
-    return read_cache()
+    def read_cache(self):
+        with open(self.cache_file, 'rb') as f:
+            return [parse_line(line.decode('utf-8')) for line in f.readlines()]
+
+    def fetch_from_github(self):
+        print("fetching starred repos of %s" % self.username)
+        with open(self.cache_file, 'wb') as f:
+            page = 1
+            while True:
+                resp = fetch_page(self.username, page)
+                page += 1
+                starred_repos = json.load(resp)
+                if len(starred_repos) == 0:
+                    print(page)
+                    break
+                for repo in starred_repos:
+                    f.write(("%s %s %s\n" %
+                             (repo['full_name'], repo['html_url'], ""
+                              if repo['description'] is None else
+                              repo['description'])).encode('utf-8'))
 
 
 def abbr(name, desc):
     name = name[:25]
-    desc = desc[:50]
+    desc = desc[:50].strip()
     spaces = 30 - wcswidth(name)
     return name + spaces * ' ' + desc
-
-
-def fetch(username):
-    print("fetching starred repos of %s" % username)
-    with open(cache_file, 'wb') as f:
-        page = 1
-        while True:
-            resp = fetch_page(username, page)
-            page += 1
-            starred_repos = json.load(resp)
-            if len(starred_repos) == 0:
-                print(page)
-                break
-            for repo in starred_repos:
-                f.write(
-                    ("%s %s %s\n" % (repo['full_name'], repo['html_url'], ""
-                                     if repo['description'] is None else
-                                     repo['description'])).encode('utf-8'))
 
 
 def fetch_page(username, page):
@@ -68,11 +69,6 @@ def fetch_page(username, page):
         "https://api.github.com/users/%s/starred?per_page=100&page=%d" %
         (username, page))
     return resp
-
-
-def read_cache():
-    with open(cache_file, 'rb') as f:
-        return [parse_line(line.decode('utf-8')) for line in f.readlines()]
 
 
 def parse_line(line):
